66a5743a5c620149a639fa83130cb0f8c4ba9b20
Matthsworld <Matthsworld@gmail.com>
2025-03-07 18:06:32 +0100

    feat: create src/pages/About.jsx file

---
+++ poll.autoxona.com/src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">About Us</h1>
+        <p>Learn more about Poll Autoxona and our mission.</p>
+      </div>
+    );
+  };
+  
+  export default About;
