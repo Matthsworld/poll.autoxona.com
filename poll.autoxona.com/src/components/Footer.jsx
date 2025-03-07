f1916e7d36647c3fef2e9d74d01bcae5d029b8ed
Matthsworld <Matthsworld@gmail.com>
2025-03-07 19:20:16 +0100

    feat: update Footer file

--- poll.autoxona.com/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="bg-black text-white text-center p-4 mt-6">
-        <p>&copy; 2025 Poll Autoxona. All rights reserved.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
---
+++ poll.autoxona.com/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-8">
+      <p>&copy; 2025 Polls Autoxona. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+/*const Footer = () => {
+    return (
+      <footer className="bg-black text-white text-center p-4 mt-6">
+        <p>&copy; 2025 Poll Autoxona. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;*/
