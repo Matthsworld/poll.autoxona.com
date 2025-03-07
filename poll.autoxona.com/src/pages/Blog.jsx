d8b10d73e48eab98533c7ac322e56a8454efb258
Matthsworld <Matthsworld@gmail.com>
2025-03-07 18:09:19 +0100

    feat: create src/pages/Blog.jsx file

---
+++ poll.autoxona.com/src/pages/Blog.jsx
@@ -0,0 +1,10 @@
+const Blog = () => {
+    return (
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Our Blog</h1>
+        <p>Latest updates and articles about polls.</p>
+      </div>
+    );
+  };
+  
+  export default Blog;
