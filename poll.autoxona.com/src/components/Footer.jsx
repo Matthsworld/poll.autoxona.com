c6fd135a6a96a90e6ac7021b4d5cce6eb6b0c28a
Matthsworld <Matthsworld@gmail.com>
2025-03-07 18:17:17 +0100

    feat: create src/components/Footer.jsx file

---
+++ poll.autoxona.com/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-black text-white text-center p-4 mt-6">
+        <p>&copy; 2025 Poll Autoxona. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
